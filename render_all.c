6e290adf4b3a163db96234eff43114c7be721064
MangoJimbo12 <mangojimbo@icloud.com>
2024-06-09 18:57:27 +0100

    Added batch rendering

--- render_all.c
+++ render_all.c
@@ -1,12 +1,12 @@
 /*
  * Graphics Engine
  *
- * File:     render_custom.c
+ * File:     render_all.c
  * Author:   James Bray
  * Repo:     https://github.com/James-Bray19/Graphics-Engine
  *
- * A simple 3D graphics engine that reads in one or more obj files 
- * and renders them as a pgm image.
+ * A simple 3D graphics engine that reads in every obj file in the obj folder
+ * and renders them to a pgm file in the pgm folder.
  * 
  * LML repo:        https://github.com/jamesbray03/Lightweight-Matrix-Library
  * 4x4 matrices:    http://www.codinglabs.net/article_world_view_projection_matrix.aspx
@@ -19,6 +19,7 @@
 #include <math.h>
 #include <string.h>
 #include <time.h>
+#include <dirent.h>
 
 #include "lml.h"
 
@@ -34,6 +35,10 @@ typedef struct {
 } Mesh;
 
 // --------------- Function Prototypes ---------------
+
+/// @brief returns the names of all obj files in the obj folder
+/// @return array of strings containing the names of the obj files
+char **get_obj_files(int *num_objects);
 
 /// @brief copies the contents of specified obj file into mesh struct
 /// @param m destination mesh
@@ -80,6 +85,10 @@ void draw_line(Matrix *screen, int x1, int y1, int x2, int y2, double intensity)
 /// @brief draws a triangle on the screen at the specified coordinates with the specified intensity
 void draw_triangle(Matrix *screen, int x1, int y1, int x2, int y2, int x3, int y3, double line_colour, double fill_colour);
 
+/// @brief sorts faces in a mesh by z value (bubble sort)
+/// @param m mesh to order
+void z_order_tris(Mesh *m);
+
 /// @brief renders the mesh to the screen
 /// @param m mesh to render
 /// @param width screen width
@@ -90,137 +99,119 @@ void render(Mesh *m, int width, int height, int perspective, char *filename);
 // --------------- Main Loop ---------------
 
 void main() {
-    char running = 'y';
-    while (running == 'y') {
-        printf("\e[1;1H\e[2J");
-        printf("\n--------------- SCENE SETUP ---------------\n");
 
+    clock_t script_start_time = clock();
 
+    // get projection matrix
+    int width = 800, height = 800;
+    int perspective = 1; 
+    double z_near = 0.1, z_far = 10, fov = 25;
+    Matrix *projection = get_projection(width, height, fov, z_near, z_far);
 
-        printf("\nExport Settings\n");
-        char pgm_name[256]; int width, height;
+    int num_objects = 0;
+    char **obj_files = get_obj_files(&num_objects);
 
-        do { printf("\trender name: "); scanf("%s", pgm_name); } while (strlen(pgm_name) == 0);
-        do { printf("\tscreen width: "); scanf("%d", &width); } while (width <= 0);
-        do { printf("\tscreen height: "); scanf("%d", &height); } while (height <= 0);
+    // print all the obj files
+    printf("\n%d objects queued.\n\nRendering...\n\n", num_objects);
 
-        // get the export file path
-        char pgm_path[356];
-        strcpy(pgm_path, "pgm/"); strcat(pgm_path, pgm_name); strcat(pgm_path, ".pgm");
+    // render each object
+    Mesh *object = malloc(sizeof(Mesh));
+    for (int i = 0; i < num_objects; i++) {
+        printf("\t%3d. %-20s", i+1, obj_files[i]);
 
+        // create the obj file path
+        char obj_path[356];
+        sprintf(obj_path, "obj/%s.obj", obj_files[i]);
 
+        // load the obj file
+        FILE *obj_file = fopen(obj_path, "r");
+        load_obj_file(object, obj_file); fclose(obj_file);
 
-        printf("\nCamera Settings\n");
-        int perspective; double z_near, z_far, fov;
+        // print object info
+        printf("loaded...");
+        clock_t obj_start_time = clock();
 
-        char default_values = 'n';
-        do { printf("    use default camera? (y/n): "); scanf(" %c", &default_values); } 
-        while (default_values != 'y' && default_values != 'n');
-
-        if (default_values == 'y') {
-            perspective = 1; z_near = 0.1; z_far = 10; fov = 60;
-        } else {
-            do { printf("\tperspective: "); scanf("%d", &perspective); } while (perspective < 0 || perspective > 1);
-            do { printf("\tnear clipping plane: "); scanf("%lf", &z_near); } while (z_near <= 0);
-            do { printf("\tfar clipping plane: "); scanf("%lf", &z_far); } while (z_far <= 0);
-            do { printf("\tfield of view: "); scanf("%lf", &fov); } while (fov <= 0 || fov >= 180);
-        }
-        Matrix *projection = get_projection(width, height, fov, z_near, z_far);
-
-
-
-        printf("\nObject Settings");
-        Mesh **queue = malloc(sizeof(Mesh *)); int num_objects = 0;
-
-        char add_another = 'y';
-        while (add_another == 'y') {
-            num_objects++;
-
-            // create mesh object
-            queue = realloc(queue, num_objects * sizeof(Mesh *));
-            queue[num_objects - 1] = malloc(sizeof(Mesh));
-
-            printf("\n    Object %d:\n", num_objects);
-
-            // get object file
-            FILE *obj_file; char obj_name[256], obj_path[256];
-            do {
-                printf("\tobject: "); scanf("%s", obj_name);
-                strcpy(obj_path, "obj/"); strcat(obj_path, obj_name); strcat(obj_path, ".obj");
-                obj_file = fopen(obj_path, "r");
-            } while (obj_file == NULL);
-            load_obj_file(queue[num_objects - 1], obj_file); fclose(obj_file);
-
-            // get transformation values
-            char default_values = 'n';
-            do { printf("\tuse default position? (y/n): "); scanf(" %c", &default_values); } 
-            while (default_values != 'y' && default_values != 'n');
-
-            double sx, sy, sz, rx, ry, rz, tx, ty, tz;
-            if (default_values == 'y') {
-                sx = 1; sy = 1; sz = 1;
-                rx = 0; ry = 0; rz = 0;
-                tx = 0; ty = 0; tz = 3;
-            } else {
-                printf("\t    scale: "); scanf("%lf %lf %lf", &sx, &sy, &sz);
-                printf("\t    rotation: "); scanf("%lf %lf %lf", &rx, &ry, &rz);
-                printf("\t    translation: "); scanf("%lf %lf %lf", &tx, &ty, &tz);
+        // get the furthest point of the object
+        double max_distance = 0;
+        for (int i = 0; i < object->verts->rows; i++) {
+            double distance = sqrt(pow(object->verts->data[i][0], 2) + pow(object->verts->data[i][1], 2) + pow(object->verts->data[i][2], 2));
+            if (distance > max_distance) {
+                max_distance = distance;
             }
-
-            // position object in 3D space
-            Matrix *trans = identity(4);
-            trans = multiply(trans, get_scale(sx, sy, sz));
-            trans = multiply(trans, get_rotation(rx, ry, rz));
-            trans = multiply(trans, get_translation(tx, ty, tz));
-            transform(queue[num_objects - 1], trans);
-            cull_backfaces(queue[num_objects - 1]);
-
-            // move to 2D space
-            transform(queue[num_objects - 1], projection);
-
-            do {printf("\tadd another object? (y/n): "); scanf(" %c", &add_another); }
-            while (add_another != 'y' && add_another != 'n');
         }
 
+        // normalise the object
+        double sf = 1 / max_distance;
+        Matrix *trans = get_scale(sf, sf, sf);
+        trans = multiply(get_rotation(-90, 0, 0), trans);
+        trans = multiply(get_rotation(0, 45, 0), trans);
+        trans = multiply(get_rotation(-25, 0, 0), trans);
+        trans = multiply(get_translation(0, 0, 5), trans);
 
 
-        printf("\n--------------- FINISHED SETUP ---------------\n");
-        printf("\nPress any key to render..."); getchar(); getchar();
+        // apply transformations
+        transform(object, trans);
+        cull_backfaces(object);
+        transform(object, projection);
+        z_order_tris(object);
 
 
+        // create the pgm file path
+        char pgm_path[356];
+        sprintf(pgm_path, "pgm/%s.pgm", obj_files[i]);
 
         // create the pgm file
         FILE *pgm_file = fopen(pgm_path, "w");
-        fprintf(pgm_file, "P2\n%d %d\n255\n", width, height);
-        fclose(pgm_file);
+        fprintf(pgm_file, "P2\n%d %d\n255\n", width, height); fclose(pgm_file);   
+            
 
-        // start timing
-        clock_t start_time = clock();
+        // render the object
+        render(object, width, height, perspective, pgm_path);
 
-        // render objects into the file
-        for (int i = 0; i < num_objects; i++) { render(queue[i], width, height, perspective, pgm_path); }
-
-        // end timing
-        clock_t end_time = clock();
-        double total_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-
-        printf("\nScene rendered to %s\n", pgm_path);
-        printf("Total time taken: %.2f seconds\n", total_time);
-
-        // clean up
-        for (int i = 0; i < num_objects; i++) {
-            release(queue[i]->verts); release(queue[i]->norms);
-            release(queue[i]->faces); release(queue[i]->face_norms);
-            free(queue[i]);
-        }
-    
-        // ask to run again
-        printf("\nRender another object? (y/n): ");
-        scanf(" %c", &running);
+        clock_t obj_end_time = clock();
+        double render_time = (double)(obj_end_time - obj_start_time) / CLOCKS_PER_SEC;
+        printf("\trendered (%d tris in %.2fs)\n", object->faces->rows, render_time);
     }
+    free(object);
+
+    printf("\nAll objects rendered successfully.");
+    clock_t script_end_time = clock();
+    double total_time = (double)(script_end_time - script_start_time) / CLOCKS_PER_SEC;
+    printf("\n(%d objects in %.2fs, avg: %.2fs)\n\n", num_objects, total_time, total_time / num_objects);
 }
 
 // --------------- Function Definitions ---------------
+
+char** get_obj_files(int *num_objects) {
+    // return every obj file in the obj folder
+    DIR *d;
+    struct dirent *dir;
+    d = opendir("obj");
+
+    // count number of obj files
+    *num_objects = 0;
+    while ((dir = readdir(d)) != NULL) {
+        if (strstr(dir->d_name, ".obj") != NULL) { (*num_objects)++; }
+    }
+    closedir(d);
+
+    // create array of strings to store file names
+    char **obj_files = malloc((*num_objects + 1) * sizeof(char*));
+    for (int i = 0; i < *num_objects + 1; i++) { obj_files[i] = malloc(256); }
+
+    // store file names in array
+    d = opendir("obj");
+    int idx = 0;
+    while ((dir = readdir(d)) != NULL) {
+        if (strstr(dir->d_name, ".obj") != NULL) {
+            char *file_name = strtok(dir->d_name, ".");
+            strcpy(obj_files[idx], file_name);
+            idx++;
+        }
+    }
+    closedir(d);
+    return obj_files;
+}
 
 void load_obj_file(Mesh *m, FILE *f) {
     
@@ -271,15 +262,42 @@ void load_obj_file(Mesh *m, FILE *f) {
         
         // if line is a face (group of indices)
         else if (line[0] == 'f') {
+            int reads = 0;
 
-            // read in vertices and normals, ignore textures for now
-            sscanf(line, "f %lf//%lf %lf//%lf %lf//%lf", 
-            &m->faces->data[face_idx][0], &m->face_norms->data[face_idx][0],
-            &m->faces->data[face_idx][1], &m->face_norms->data[face_idx][1],
-            &m->faces->data[face_idx][2], &m->face_norms->data[face_idx][2]);
-            face_idx++;
+            if (reads != 6) {
+                // first attempt: trying to read with vertex/texture/normal format
+                reads = sscanf(line, "f %lf/%*lf/%lf %lf/%*lf/%lf %lf/%*lf/%lf", 
+                            &m->faces->data[face_idx][0], &m->face_norms->data[face_idx][0],
+                            &m->faces->data[face_idx][1], &m->face_norms->data[face_idx][1],
+                            &m->faces->data[face_idx][2], &m->face_norms->data[face_idx][2]);
+            }
+
+            if (reads != 6) {
+                // second attempt: trying to read with vertex//normal format
+                reads = sscanf(line, "f %lf//%lf %lf//%lf %lf//%lf", 
+                            &m->faces->data[face_idx][0], &m->face_norms->data[face_idx][0],
+                            &m->faces->data[face_idx][1], &m->face_norms->data[face_idx][1],
+                            &m->faces->data[face_idx][2], &m->face_norms->data[face_idx][2]);
+            }
+
+            if (reads != 6) {
+                // third attempt: trying to read with vertex texture vertex texture vertex texture format
+                reads = sscanf(line, "f %lf %*lf %lf %lf %*lf %lf %lf %*lf %lf", 
+                            &m->faces->data[face_idx][0], &m->face_norms->data[face_idx][0],
+                            &m->faces->data[face_idx][1], &m->face_norms->data[face_idx][1],
+                            &m->faces->data[face_idx][2], &m->face_norms->data[face_idx][2]);
+            }
+
+            if (reads == 6) { 
+                face_idx++; 
+            } else { 
+                printf("Error reading face: %s", line); 
+            }
+
         }
+        else { continue; }
     }
+
     fclose(f);
 }
 
@@ -482,9 +500,9 @@ void draw_triangle(Matrix *screen, int x1, int y1, int x2, int y2, int x3, int y
         draw_line(screen, x_start, y, x_end, y, fill_colour);
     }
 
-    draw_line(screen, x1, y1, x2, y2, line_colour);
-    draw_line(screen, x2, y2, x3, y3, line_colour);
-    draw_line(screen, x3, y3, x1, y1, line_colour);
+    // draw_line(screen, x1, y1, x2, y2, line_colour);
+    // draw_line(screen, x2, y2, x3, y3, line_colour);
+    // draw_line(screen, x3, y3, x1, y1, line_colour);
 }
 
 void buffer_to_pgm(Matrix *screen, int width, int height, char *filepath) {
@@ -528,11 +546,90 @@ void buffer_to_pgm(Matrix *screen, int width, int height, char *filepath) {
     fclose(pgm_file);
 }
 
+void swap_faces(Mesh *m, int i, int j) {
+    // swap faces
+    for (int k = 0; k < m->faces->cols; k++) {
+        double temp = m->faces->data[i][k];
+        m->faces->data[i][k] = m->faces->data[j][k];
+        m->faces->data[j][k] = temp;
+    }
+
+    // swap face normals
+    for (int k = 0; k < m->face_norms->cols; k++) {
+        double temp = m->face_norms->data[i][k];
+        m->face_norms->data[i][k] = m->face_norms->data[j][k];
+        m->face_norms->data[j][k] = temp;
+    }
+}
+
+void z_order_tris(Mesh *m) {
+    // calculate average z value of each face
+    double *z_avg = calloc(m->faces->rows, sizeof(double));
+    
+    for (int i = 0; i < m->faces->rows; i++) {
+        // get vertex coordinates
+        double z1 = m->verts->data[(int)m->faces->data[i][0] - 1][2];
+        double z2 = m->verts->data[(int)m->faces->data[i][1] - 1][2];
+        double z3 = m->verts->data[(int)m->faces->data[i][2] - 1][2];
+
+        // calculate furthest z value
+        // z_avg[i] = fmax(z1, fmax(z2, z3));
+
+        // calculate average z value
+        z_avg[i] = (z1 + z2 + z3) / 3;
+    }
+
+    // sort faces by z value (bubble sort)
+    int temp;
+    for (int i = 0; i < m->faces->rows; i++) {
+        for (int j = i + 1; j < m->faces->rows; j++) {
+            if (z_avg[i] < z_avg[j]) {
+
+                // swap z values and faces
+                temp = z_avg[i]; 
+                z_avg[i] = z_avg[j]; 
+                z_avg[j] = temp;
+                
+                swap_faces(m, i, j);
+            }
+        }
+    }
+
+    // free allocated memory for z_avg
+    free(z_avg);
+}
+
+// will return to this, for now it's just blurring everyhting
+
+// void apply_anti_aliasing(Matrix *screen, int threshold) {
+//     // get a buffer containing all of the edges using edge detection
+//     Matrix *edges = zeros(screen->rows, screen->cols);
+
+//     Matrix *neighbours = zeros(3, 3);
+//     for (int i = 1; i < screen->rows - 1; i++) {
+//         for (int j = 1; j < screen->cols - 1; j++) {
+//             neighbours = get_submatrix(screen, i - 1, j - 1, 3, 3);
+//             double edge_intensity = neighbours->data[1][1];
+//             double sum = 0;
+//             for (int k = 0; k < neighbours->rows; k++) {
+//                 for (int l = 0; l < neighbours->cols; l++) {
+//                     edge_intensity += neighbours->data[1][1] - neighbours->data[k][l];
+//                     sum += neighbours->data[k][l];
+//                 }
+//             }
+//             edges->data[i][j] = edge_intensity / 9;
+//             if (edges->data[i][j] > threshold) { screen->data[i][j] = sum / 9; }
+//         }
+//     }
+//     free(neighbours);
+//     free(edges);
+// }
+
 void render(Mesh *m, int width, int height, int perspective, char *filepath) {
 
     // create screen buffer
     Matrix *screen = zeros(height, width);
-  
+
     // convert each vertex to screen space
     for (int i = 0; i < m->verts->rows; i++) {
 
@@ -561,8 +658,11 @@ void render(Mesh *m, int width, int height, int perspective, char *filepath) {
         int x3 = m->verts->data[v3][0]; int y3 = m->verts->data[v3][1];
 
         // fill triangle and draw edges (in rendering order)
-        draw_triangle(screen, x1, y1, x2, y2, x3, y3, 1, get_light_intensity(m, i));
+        draw_triangle(screen, x1, y1, x2, y2, x3, y3, 0, get_light_intensity(m, i));
     }
+
+    // apply FXAA
+    // apply_anti_aliasing(screen, 0.9);
 
     // write the buffer to a pgm file
     buffer_to_pgm(screen, width, height, filepath);
